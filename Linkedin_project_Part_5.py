05b80dd212b737a145fbbc602d521f736d0ae09a
kaeest <kae@aals.ch>
2021-04-25 19:26:39 +0200

    heroku chromdriver

--- Linkedin_project_Part_5.py
+++ Linkedin_project_Part_5.py
@@ -29,7 +29,7 @@ def scrape(search_query, password, username):
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") #GOOGLE_CHROME_BIN
     chrome_options.add_argument ('--disable-gpu')
     chrome_options.add_argument ('--no-sandbox')
-    driver = webdriver.Chrome (executable_path=CHROMEDRIVER_PATH , chrome_options=chrome_options)
+    driver = webdriver.Chrome (executable_path=os.environ.get("CHROMEDRIVER_PATH") , chrome_options=chrome_options)
     # Parameter for Heroku End
 
 
