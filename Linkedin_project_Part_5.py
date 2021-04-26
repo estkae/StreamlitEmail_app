316e5c85dff0260f8fdf410b25a4336a9b82b525
kaeest <kae@aals.ch>
2021-04-26 09:03:42 +0200

    chrome driver option

--- Linkedin_project_Part_5.py
+++ Linkedin_project_Part_5.py
@@ -29,6 +29,7 @@ def scrape(search_query, password, username):
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") #GOOGLE_CHROME_BIN
     chrome_options.add_argument ('--disable-gpu')
     chrome_options.add_argument ('--no-sandbox')
+    chrome_options.add_argument ("--remote-debugging-port=9222")  # this
     driver = webdriver.Chrome (executable_path=os.environ.get("CHROMEDRIVER_PATH") , chrome_options=chrome_options)
     # Parameter for Heroku End
 
